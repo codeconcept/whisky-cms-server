c6389cbc2dc9302a8039d65763043de0ed42ccd0
codeconcept <contact@codeconcept.fr>
2018-10-14 12:08:35 +0200

    fixing CORS issues

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const api = require('./api');
 
+// cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.set('port', (process.env.PORT || 8081));
 
 app.use(bodyParser.json());
